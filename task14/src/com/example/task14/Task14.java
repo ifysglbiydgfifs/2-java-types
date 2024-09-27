bda93d12e770de887e418c518ffeb0a07efe98ae
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2024-09-27 05:35:49 +0500

    Мустафьев Никита ПрИ-201. Task14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -5,9 +5,8 @@ public class Task14 {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        StringBuilder stb = new StringBuilder(Integer.toString(value));
+        return Integer.parseInt(stb.reverse().toString());
     }
 
     public static void main(String[] args) {
